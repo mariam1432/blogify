103e1d9e66b2b9e64fa9965de3b9a441e944916b
mariam4132 <mariam.fatimaa007@gmail.com>
2023-10-09 09:58:02 +0500

    future

--- about.php
+++ about.php
@@ -6,7 +6,7 @@ include('partials/header.php')
 <section class="empty__page">
     <h1>About</h1>
 </section>
-
+<!-- future: add new feature to display about section for the author -->
 <script src="js\main.js"></script>
 
 <?php
